4876625f3fb3ad4c0ab5de1100e6bf2bf19fce0c
Gyarmati Csanád <gyarmati.csanad@burg.lo.hu>
2024-09-19 14:25:12 +0200

    first commit

---
+++ Konyv.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konyv
+{
+    internal class Konyv
+    {
+        string cim, szerzo, mufaj;
+        int kiadasDatuma, oldalszam;
+
+        public Konyv(string cim, string szerzo, string mufaj, int kiadasDatuma, int oldalszam)
+        {
+            this.cim = cim;
+            this.szerzo = szerzo;
+            this.mufaj = mufaj;
+            this.kiadasDatuma = kiadasDatuma;
+            this.oldalszam = oldalszam;
+        }
+
+        public string Cim { get => cim; set => cim = value; }
+        public string Szerzo { get => szerzo; set => szerzo = value; }
+        public string Mufaj { get => mufaj; set => mufaj = value; }
+        public int KiadasDatuma { get => kiadasDatuma; set => kiadasDatuma = value; }
+        public int Oldalszam { get => oldalszam; set => oldalszam = value; }
+
+        public Konyv(string fajlNev)
+        {
+            string[] sor = fajlNev.Split(',');
+            this.cim = sor[0];
+            this.szerzo = sor[1];
+            this.mufaj = sor[4];
+            this.kiadasDatuma =int.Parse(sor[2]);
+            this.oldalszam = int.Parse(sor[3]);
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Konyv
+{
+    internal class Program
+    {
+
+        static List<Konyv> konyvek = new List<Konyv>();
+        static void Main(string[] args)
+        {
+            var adat = File.ReadAllLines("konyvek.txt");
+            foreach (var item in adat)
+            {
+                konyvek.Add(new Konyv(item));
+            }
+            SzerzoKereso();
+            LegnagyobbOldal();
+            KiadasEvSzerint();
+            KonyvSzam();
+            KonyvSzamOsszes();
+        }
+
+
+        static void SzerzoKereso()
+        {
+            Console.Write("Keressen ra egy szerzo nevere: ");
+            string keresettSzerzo = Console.ReadLine();
+            Console.WriteLine($"\nA keresett szerző által írt könyvek:");
+            foreach (var item in konyvek)
+            {
+                var elem = item.Szerzo;
+                if (keresettSzerzo == elem)
+                {
+                    Console.WriteLine($"-{item.Cim}");
+                }
+            }
+
+
+        }
+
+        static void LegnagyobbOldal()
+        {
+            int legtobb = 0;
+            string cim = konyvek[0].Cim;
+
+            foreach (var item in konyvek)
+            {
+                if (item.Oldalszam > legtobb)
+                {
+                    legtobb = item.Oldalszam;
+                    cim = item.Cim;
+                }
+
+            }
+            Console.WriteLine($"\nA könyv címe a legtöbb oldalszámmal: {cim}, oldalszám: {legtobb}\n");
+        }
+
+        static void KiadasEvSzerint()
+        {
+            List<Konyv> konyvRendezes = konyvek.OrderBy(x => x.KiadasDatuma).ToList();
+
+            foreach (var konyv in konyvRendezes)
+            {
+                Console.WriteLine($"-{konyv.Cim}");
+            }
+        }
+
+        static void KonyvSzam()
+        {
+            Console.Write("\nKeress ra egy mufajra: ");
+            string keresettMufaj = Console.ReadLine();
+            int talalt = 0;
+            foreach (var item in konyvek)
+            {
+                var elem = item.Mufaj;
+                if (keresettMufaj == elem)
+                {
+                    talalt++;
+                }
+            }
+            Console.WriteLine($"\nA keresett műfajra talált könyvek száma: {talalt}");
+
+        }
+
+        static void KonyvSzamOsszes()
+        {
+            var mufajok = konyvek.GroupBy(k => k.Mufaj);
+
+            foreach (var mufaj in mufajok)
+            {
+                Console.WriteLine($"\nMűfaj: {mufaj.Key}, Könyvek száma: {mufaj.Count()}");
+                foreach (var konyv in mufaj)
+                {
+                    Console.WriteLine($"  Cím: {konyv.Cim}, Szerző: {konyv.Szerzo}, Kiadás dátuma: {konyv.KiadasDatuma}, Oldalszám: {konyv.Oldalszam}");
+                }
+            }
+        }
+    }
+}
